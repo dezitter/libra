1232c193f4654a137decb844c479a8e6531b0400
Laurent Dezitter <laurent.dezitter@gmail.com>
2015-11-20 14:51:59 +0100

    remove unused home page

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,8 @@
 export default function(match) {
-    match('/'             , 'home#index');
-
     match('/login'        , 'user#login');
     match('/signup'       , 'user#signup');
 
+    match('/'             , 'files#index');
     match('/files'        , 'files#index');
     match(/files(\/.+)/   , 'files#show');
 }
